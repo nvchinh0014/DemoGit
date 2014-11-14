5ce900cb0d12aeb4549f822d785b809cda4346ca
nvchinh <vchinh.fan@gmail.com>
2014-11-14 21:48:29 +0700

    new project

---
+++ MyProject/Default.aspx.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace MyProject
+{
+    public partial class _Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            int a;
+            int b;
+            a = 45;
+            b = 90;
+        }
+    }
+}
